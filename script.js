6c3313ebc7c133c468a1992e3dd4269a5408eb82
vivian-ms <56147408+vivian-ms@users.noreply.github.com>
2021-03-21 22:06:42 -0700

    Added function to append svg element to #svg_container

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
 const resource = 'https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/cyclist-data.json';
+const w = 800;
+const h = 400;
+const xPadding = 100;
+const yPadding = 50;
+
 
 window.onload = () => {
   fetch(resource)
     .then(response => response.json())
     .then(data => {
-      console.log(data);
+      createCanvas(data);
     })
     .catch(err => {
       console.log(`Error: ${err}`);
     });
 };
+
+
+function createCanvas(data) {
+  const svg = d3.select('#svg_container')
+                .append('svg')
+                .attr('width', w + 2 * xPadding)
+                .attr('height', h + 2 * yPadding)
+                .append('g')
+                .attr('transform', `translate(${xPadding}, ${yPadding / 2})`);
+}
